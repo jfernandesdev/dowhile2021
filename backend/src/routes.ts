418debb2add145c8f18c82072457e4623ba10606
jfernandesdev <jfernandes.dev@gmail.com>
2021-10-19 10:29:45 -0300

    route to get the profile user

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
 import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 const router = Router();
@@ -10,6 +11,8 @@ router.post("/authenticate", new AuthenticateUserController().handle);
 
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
 
-router.get("/messages/last3", new GetLast3MessagesController().handle)
+router.get("/messages/last3", new GetLast3MessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router }
