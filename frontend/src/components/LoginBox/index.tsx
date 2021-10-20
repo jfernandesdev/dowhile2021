18ebda0786afe02e5a9b313a8504c754bc7b8bfa
jfernandesdev <jfernandes.dev@gmail.com>
2021-10-20 09:54:23 -0300

    creation of authentication, login and logout context

--- frontend/src/components/LoginBox/index.tsx
+++ frontend/src/components/LoginBox/index.tsx
@@ -1,12 +1,15 @@
-import { VscGithubInverted } from 'react-icons/vsc';
+import { useContext} from 'react';
+import { AuthContext } from '../../contexts/auth';
 
+import { VscGithubInverted } from 'react-icons/vsc';
 import styles from './styles.module.scss';
 
 export function LoginBox() {
+  const {signInUrl} = useContext(AuthContext);
   return(
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="20" />
         Entrar com Github
       </a>
