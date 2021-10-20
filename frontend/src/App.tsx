2a6fc2f24022d918cbc0199aaa53b2508ee27f3a
jfernandesdev <jfernandes.dev@gmail.com>
2021-10-20 11:08:32 -0300

    component creation: send message form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
+import { useContext } from 'react';
+import { AuthContext } from './contexts/auth';
+
 import { MessageList } from './components/MessageList';
 import { LoginBox } from './components/LoginBox';
+import { SendMessageForm } from './components/SendMessageForm';
 
 import styles from './App.module.scss';
 
 export function App() {
+  const { user } = useContext(AuthContext);
+
   return (
     <main className={styles.contentWrapper}>
       <MessageList />
-      <LoginBox />
+
+      {!!user ? <SendMessageForm /> : <LoginBox /> }
     </main>
   )
 }
---
+++ frontend/src/components/SendMessageForm/index.tsx
@@ -0,0 +1,43 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/auth';
+
+import { VscGithubInverted, VscSignOut } from 'react-icons/vsc';
+import styles from './styles.module.scss';
+
+export function SendMessageForm() {
+  const { user, signOut } = useContext(AuthContext);
+
+  return(
+    <div className={styles.sendMessageFormWrapper}>
+      <button 
+        className={styles.signOutButton}
+        onClick={signOut}
+      >
+        <VscSignOut size={32}/>
+      </button>
+
+      <header className={styles.userInformation}>
+        <div className={styles.userImage}>
+          <img src={user?.avatar_url} alt={user?.name} />
+        </div>
+        <strong className={styles.userName}>{user?.name}</strong>
+        <span className={styles.userGithub}>
+          <VscGithubInverted size={16} />
+          {user?.login}</span>
+      </header>
+
+      <form className={styles.sendMessageForm}>
+        <label htmlFor="message">Mensagem</label>
+        <textarea 
+          name="message" 
+          id="message" 
+          placeholder="Qual sua expectativa para o evento?"
+          required
+          maxLength={250}
+        />
+
+        <button type="submit">Enviar mensagem</button>
+      </form>
+    </div>
+  );
+}
