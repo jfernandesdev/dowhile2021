bce804d0a54486a44aedae865e1af594b4b10be5
jfernandesdev <jfernandes.dev@gmail.com>
2021-10-21 15:02:25 -0300

    upload of project assets

---
+++ app/src/screens/Home/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
