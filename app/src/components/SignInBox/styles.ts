769b57f266b4a4fda445a4160bdf1263a21be8fa
jfernandesdev <jfernandes.dev@gmail.com>
2021-10-21 21:07:47 -0300

    update component signInBox

--- app/src/components/SignInBox/styles.ts
+++ app/src/components/SignInBox/styles.ts
@@ -1,9 +1,12 @@
 import { StyleSheet } from 'react-native';
-import { COLORS, FONTS } from '../../theme';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const styles = StyleSheet.create({
   container: {
-   paddingVertical: 30,
-   paddingHorizontal: 20,
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingBottom: getBottomSpace() + 32
   },
 });
