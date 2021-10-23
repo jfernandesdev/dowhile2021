bbf791a9ec08045bc29627b2014d5e84c6fec9d0
Jeferson Fernandes <89709787+jfernandesdev@users.noreply.github.com>
2021-10-23 20:29:01 -0300

    Update URL of API

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://api-dowhile2021.herokuapp.com',
+  baseURL: 'https://api-dowhile2021-jfernandesdev.herokuapp.com/',
 })
