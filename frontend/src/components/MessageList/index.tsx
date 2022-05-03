5a7824eab84c60491ea237dcdabfb71d9afb42dd
Jeferson Fernandes <jfernandes.dev@gmail.com>
2022-05-03 14:43:02 -0300

    Update index.tsx

--- frontend/src/components/MessageList/index.tsx
+++ frontend/src/components/MessageList/index.tsx
@@ -61,7 +61,7 @@ export function MessageList() {
 
             <div className={styles.messageUser}>
               <div className={styles.userImage}>
-                <img src={message.user.avatar_url} />
+                {(message.user.avatar_url) ? <img src={message.user.avatar_url} /> : '';}
               </div>
               <span>{message.user.name}</span>
             </div>
